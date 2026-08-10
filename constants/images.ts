a1f66a3a1dc4e9a3a9051dcde522603a586072f7
Tin Hon Ng <tinhonng@gmail.com>
2026-08-09 17:54:17 -0700

    add space theme

--- constants/images.ts
+++ constants/images.ts
@@ -1,8 +1,11 @@
+import astronaut1 from "@/assets/images/astronaut-1.png";
+import astronaut2 from "@/assets/images/astronaut-2.png";
 import girlCatReading1 from "@/assets/images/girl_cat_reading_1.png";
 import girlCatReading2 from "@/assets/images/girl_cat_reading_2.png";
 import girlCoffeeCat1 from "@/assets/images/girl_coffee_cat_1.png";
 import girlCoffeeCat2 from "@/assets/images/girl_coffee_cat_2.png";
 import snowCabinBackground from "@/assets/images/snow_cabin_background.png";
+import spaceTravelBackground from "@/assets/images/space-travel.png";
 import splashMascot from "@/assets/images/splash-mascot.png";
 import springMeadowBackground from "@/assets/images/spring_meadow.png";
 
@@ -14,4 +17,7 @@ export const images = {
   snowCabinBackground,
   girlCoffeeCat1,
   girlCoffeeCat2,
+  spaceTravelBackground,
+  astronaut1,
+  astronaut2,
 };
