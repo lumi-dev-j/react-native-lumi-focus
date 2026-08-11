86ce92333c5843c80681328293239fbeacc77d01
Tin Hon Ng <tinhonng@gmail.com>
2026-08-10 23:40:02 -0700

    add Halloween theme

--- constants/audios.ts
+++ constants/audios.ts
@@ -1,4 +1,5 @@
 import end from "@/assets/audios/end.mp3";
+import halloweenRide from "@/assets/audios/halloween-ride.mp3";
 import snowCabin from "@/assets/audios/snow-cabin.mp3";
 import spaceTravel from "@/assets/audios/space-travel.mp3";
 import springMeadow from "@/assets/audios/spring-meadow.mp3";
@@ -7,5 +8,6 @@ export const audios = {
   snowCabin,
   springMeadow,
   spaceTravel,
+  halloweenRide,
   end,
 };
