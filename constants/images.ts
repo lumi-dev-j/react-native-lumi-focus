4a7f7f127c76210994b2d4999093d295242c806b
Tin Hon Ng <tinhonng@gmail.com>
2026-08-11 17:32:39 -0700

    add themes

--- constants/images.ts
+++ constants/images.ts
@@ -2,11 +2,20 @@ import astronaut1 from "@/assets/images/astronaut-1.png";
 import astronaut2 from "@/assets/images/astronaut-2.png";
 import girlCatReading1 from "@/assets/images/girl_cat_reading_1.png";
 import girlCatReading2 from "@/assets/images/girl_cat_reading_2.png";
+import girlCatSeaside1 from "@/assets/images/girl-cat-seaside-1.png";
+import girlCatSeaside2 from "@/assets/images/girl-cat-seaside-2.png";
 import girlCatSweep1 from "@/assets/images/girl_cat_sweep_1.png";
 import girlCatSweep2 from "@/assets/images/girl_cat_sweep_2.png";
+import girlCatWalk1 from "@/assets/images/girl-cat-walk-1.png";
+import girlCatWalk2 from "@/assets/images/girl-cat-walk-2.png";
 import girlCoffeeCat1 from "@/assets/images/girl_coffee_cat_1.png";
 import girlCoffeeCat2 from "@/assets/images/girl_coffee_cat_2.png";
+import girlCatDive1 from "@/assets/images/girl-cat-dive-1.png";
+import girlCatDive2 from "@/assets/images/girl-cat-dive-2.png";
+import greekSeasideBackground from "@/assets/images/greek-seaside.png";
 import halloweenRideBackground from "@/assets/images/halloween_ride.png";
+import oceanAdventureBackground from "@/assets/images/ocean-adventure.png";
+import parisRiversideBackground from "@/assets/images/paris-riverside.png";
 import snowCabinBackground from "@/assets/images/snow_cabin_background.png";
 import spaceTravelBackground from "@/assets/images/space-travel.png";
 import splashMascot from "@/assets/images/splash-mascot.png";
@@ -26,4 +35,13 @@ export const images = {
   halloweenRideBackground,
   girlCatSweep1,
   girlCatSweep2,
+  parisRiversideBackground,
+  girlCatWalk1,
+  girlCatWalk2,
+  greekSeasideBackground,
+  girlCatSeaside1,
+  girlCatSeaside2,
+  oceanAdventureBackground,
+  girlCatDive1,
+  girlCatDive2,
 };
